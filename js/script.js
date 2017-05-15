79908aa44174530f9cf8da003229322f8fe32298
Martin <martinconradpelayo@gmail.com>
2017-05-15 11:27:39 -0700

    resubmit of project

--- js/script.js
+++ js/script.js
@@ -17,20 +17,30 @@ let quotes = [
     quote: 'How wude!',
     source: 'JarJar Binks',
     citation: 'StarWars: Episode I'
+  },
+  {
+    quote: 'Only a Sith Lord deals in absolutes.',
+    source: 'OBI-WAN KENOBI',
+    citation: 'Star Wars Episode V'
+  },
+  {
+    quote: 'Do... or do not. There is no try.',
+    source: 'Yoda',
+    citation: 'StarWars: Episode V'
   }
 ];
 
 function getRandomQuote() { //gets a random number and uses it as a random index for the quotes' objects, then returns the random object
-  let randNum = Math.floor(Math.random() * (3 - 0)) + 0;
+  let randNum = Math.floor(Math.random() * (5 - 0)) + 0;
   
   return quotes[randNum];
 }
 
 function printQuote() { //Calls getRandomQuote, uses return data to construct strings and gets/changes pertanant elements
   let randObj = getRandomQuote(); 
-  let test1 = document.getElementsByClassName('qoute').innerHTML = '<p> "' + randObj.quote + '"</p>';
-  let test2 = document.getElementsByClassName('source').innerHTML = '<p>' + randObj.source + '</p>';
-  let test3 = document.getElementsByClassName('citation').innerHTML = '<p>' + randObj.citation + '</p>';
+  let test1 = document.getElementsByClassName('qoute').innerHTML = '<p class="quote">' + randObj.quote + '</p>';
+  let test2 = document.getElementsByClassName('source').innerHTML = '<p class="source">' + randObj.source;
+  let test3 = document.getElementsByClassName('citation').innerHTML = '<span class="citation">' + randObj.citation + '</span></p>';
   
   document.getElementById('quote-box').innerHTML = test1 + test2 + test3;
 }
